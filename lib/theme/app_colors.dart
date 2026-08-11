a7579a1d0e26165ae4f3d2f5d0395de3b71e660e
Sonam-Jigme-Chelzang <cihe240156@student.cihe.edu.au>
2026-08-12 09:23:06 +1000

    GS-008 update Glassnik design system

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
 
+class AppColors {
+  static const Color primary = Color(0xFF6C63FF);
+  static const Color secondary = Color(0xFF00D4FF);
+
+  static const Color background = Color(0xFF0D0D0D);
+  static const Color surface = Color(0xFF1A1A1A);
+
+  static const Color textPrimary = Colors.white;
+  static const Color textSecondary = Color(0xFFAAAAAA);
+}
