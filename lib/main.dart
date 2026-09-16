1625a4ecb493bbbee4eb86675fbe245ec083188b
Jigdrel <jigdrel1999dorji@gmail.com>
2026-09-16 10:20:54 +1000

    Add Firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ import 'screens/splash_screen.dart';
 import 'services/theme_store.dart';
 import 'theme/app_theme.dart';
 import 'widgets/iphone_frame.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const GlassnikApp());
-}
+Future<void> main() async { 
+  WidgetsFlutterBinding.ensureInitialized(); await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform); runApp(const GlassnikApp()); }
 
 class GlassnikApp extends StatelessWidget {
   const GlassnikApp({super.key});
